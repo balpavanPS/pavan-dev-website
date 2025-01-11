4992e7db3967f0150bcee9928eb6518094867c22
Balivada Gnan Pavan <gnanp357@gmail.com>
2025-01-11 18:17:24 +0530

    make add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,8 +48,33 @@ products.forEach((product) => {
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">Add to Cart</button>
+        <button class="add-to-cart-button button-primary js-add-to-cart-btn" data-product-id = "${
+          product.id
+        }">Add to Cart</button>
     </div>`;
 });
 
 document.querySelector(".products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
