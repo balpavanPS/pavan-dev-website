338d331fc5ab4b82e6aaaed50d655b1f0cb00119
Balivada Gnan Pavan <gnanp357@gmail.com>
2025-01-12 16:00:20 +0530

    added first test file

---
+++ jasmine-standalone-5.1.1/MoneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: format currency", () => {
+  it("converts the cents to dollars", () => {
+    expect(formatCurrency(1095)).toEqual("10.95");
+  });
+
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("numbers rounded to nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  });
+});
