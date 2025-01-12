f0c82d9655c513a13837ce0aa2607584568f2d8a
Balivada Gnan Pavan <gnanp357@gmail.com>
2025-01-12 21:57:31 +0530

    added backend practice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import RenderOrderSummary from "./checkout/orderSummary.js";
 import RenderPaymentSummary from "./checkout/paymentSummary.js";
 import RenderCheckOutHeader from "./checkout/checkOutHeader.js";
 import "../data/cart-class.js";
+import "../data/backend-practice.js";
 
 RenderCheckOutHeader();
 RenderOrderSummary();
