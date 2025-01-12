04a442908f6e9c328ccd377aa78a6acbbb6a2398
Balivada Gnan Pavan <gnanp357@gmail.com>
2025-01-12 18:34:38 +0530

    cart-oops

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import RenderOrderSummary from "./checkout/orderSummary.js";
 import RenderPaymentSummary from "./checkout/paymentSummary.js";
 import RenderCheckOutHeader from "./checkout/checkOutHeader.js";
+import "../data/cart-oop.js";
 
 RenderCheckOutHeader();
 RenderOrderSummary();
