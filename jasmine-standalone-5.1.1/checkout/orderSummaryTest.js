4bdb18d4bd99d3c2cf1d049d5e06808ecfc5f269
Balivada Gnan Pavan <gnanp357@gmail.com>
2025-01-12 22:25:14 +0530

    fixes for orderSummaryTest

--- jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
+++ jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
@@ -2,11 +2,19 @@ import RenderOrderSummary from "../../scripts/checkout/orderSummary.js";
 // import { loadFromStorage, cart } from "../../data/cart.js";
 // import Cart from "../../data/cart-class.js";
 
+import { loadProducts } from "../../data/products.js";
+
 describe("test suite: render order summary", () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
   // const cartObj = new Cart("cart-test");
   // const cart = cartObj.cartItems;
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
   beforeEach(() => {
     spyOn(localStorage, "setItem");
     document.querySelector(".js-test-container").innerHTML = `
